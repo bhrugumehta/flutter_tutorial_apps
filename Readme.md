0c9709a745caeb476dd01c2e6d220eb6a040cfe2
Bhrugu <bhrugumehta@gmail.com>
2018-09-08 22:55:38 +0530

    first hello,world

---
+++ Readme.md
@@ -0,0 +1,1 @@
+1) minimal_app : it's flutter minimal app to run on device like hello, world in every programm`
---
+++ minimal_app/lib/main.dart
@@ -0,0 +1,18 @@
+// material.dart is required for various widgets like
+// Container, Material design i.e. Flutter SDK
+
+import 'package:flutter/material.dart';
+
+//main () is the entry point, main() is required by dart
+//you can here, omit the void in front of main, default is void
+main() {
+  //runApp() provided by the material.dart
+  //which loads/run the app
+  //require widget as parameter
+  //Here, Container is the widget
+  //So, at least one widget required to make flutter app because everything in
+  //flutter is widget !!!
+  runApp(Container());
+}
+
+
